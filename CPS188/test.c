3790a5edfdb835f30b449b901f101a247de7af09
Kevin <k10li@torontomu.ca>
2026-01-15 12:10:43 -0500

    first commit

---
+++ CPS188/test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    //double num;
+    printf("Hello, World!\n");
+    //scanf("%lf", &num);
+    //printf("You entered: %lf\n", num);
+    int num;
+    scanf("%d", &num);
+    printf("You entered: %d\n", num);
+    getchar();
+    printf("Press Enter to exit...");
+    getchar();
+    return 0;
+}   
