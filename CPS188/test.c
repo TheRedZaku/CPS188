8cc7c68984b8d5a5e5652c2b4552c0bbd2e2819f
Kevin <k10li@torontomu.ca>
2026-01-16 15:31:20 -0500

    test is slightly more complex and includes if and while loops

--- CPS188/test.c
+++ CPS188/test.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 
-int main() {
+int main(void) {
     //double num;
     printf("Hello, World!\n");
     //scanf("%lf", &num);
     //printf("You entered: %lf\n", num);
     int num;
-    printf("Enter an integer: ");
-    scanf("%d", &num);
-    printf("You entered: %d\n", num);
+    do {
+        printf("Enter an integer: ");
+        scanf("%d", &num);
+        if (num != 67) {
+            printf("That's not 67.\n");
+        }
+    } while (num != 67);
+    printf("yay!\n");
     getchar();
     printf("Press Enter to exit...");
     getchar();
