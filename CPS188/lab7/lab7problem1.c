ecaf4c657d5b3a8e5a85fc6acf20994d39af2752
Kevin <k10li@torontomu.ca>
2026-03-12 16:03:57 -0400

    gosh i hate this recursion

--- CPS188/lab7/lab7problem1.c
+++ CPS188/lab7/lab7problem1.c
@@ -4,6 +4,10 @@
 #include <string.h>
 #include <ctype.h>
 
+#define GREEN "\033[32;1m"
+#define RED "\033[31;1m"
+#define BOLD "\033[1m"
+
 void clean(char before[], char after[]) {
 
     int modifiedIndex = 0;
@@ -18,11 +22,16 @@ void clean(char before[], char after[]) {
 
 void reverse(char before[], char after[]) {
 
+    if ((int)strlen(before) == 0) {
 
-    if ((int)strlen(after) > (int)strlen(before)) {
-        after[0] = before[(int)strlen(before) - 1];
+        after[(int)strlen(after)] = '\0';//set the last value to the null character, and exit the function
+
     } else {
-        after[0] = before[(int)strlen(before) - 1]; 
+
+        after[(int)strlen(after)] = before[(int)strlen(before) - 1];//set the current highest index of the new array to the last character in the original array
+        before[(int)strlen(before) - 1] = '\0'; //override the last value of the before array, so that the size decreases
+        after[(int)strlen(after) + 1] = '\0'; //add back a null character in case
+        reverse(before, after); //recursively calling the function again
     }
 
 }
@@ -30,15 +39,39 @@ void reverse(char before[], char after[]) {
 int main(void) {
 
     fflush(stdin);
-    char input[100], modifiedInput[100], reversedInput[100];
-    int isPalindrome = 0;
+    char input[100] = "\0", modifiedInput[100] = "\0", reversedInput[100] = "\0";
+    int isPalindrome = 1;//updates to a 0, representing NOT, if not a palindrome
 
-    printf("Enter a word: ");
+    printf(BOLD "Enter a word: ");
+
     fgets(input, 100, stdin); //input is the same as &input
-    printf("%s\n", input);
+    //printf("%s\n", input);
+
     clean(input, modifiedInput); //pointer addresses sent to function
-    printf("%s", modifiedInput);
+    printf("\nYour input without punctuation, spaces or capitalization is: %s\n", modifiedInput);
+
+    reversedInput[0] = '\0';
     reverse(modifiedInput, reversedInput);
+
+    clean(input, modifiedInput);
+
+    printf("The reverse of the given string is: %s\n", reversedInput);
+
+    for (int index = 0; index < (int)strlen(modifiedInput); index++) {//checks if every single character in both the reverse and regular string are the same
+        if (modifiedInput[index] != reversedInput[index]) {//if even one is different, it will break and update the check to 0
+            isPalindrome = 0;
+            break;
+        }
+    }
+
+    switch (isPalindrome) {
+        case 1:
+            printf(GREEN "The string: %sis a palindrome.", input);
+            break;
+        default:
+            printf(RED "The string: %sis NOT a palindrome.", input);
+            break;
+    }
 
     fflush(stdin);
     getchar();
