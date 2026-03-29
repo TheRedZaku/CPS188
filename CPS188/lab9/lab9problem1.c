a5d1cb98521c14f505a00e9e2f20fa545745fda5
Kevin <k10li@torontomu.ca>
2026-03-28 20:02:45 -0400

    yea i dont like this program

--- CPS188/lab9/lab9problem1.c
+++ CPS188/lab9/lab9problem1.c
@@ -14,8 +14,8 @@ int main(void) {
     FILE *in;
     char *token; //token for splitting the csv values
     double temperature[ROWS][COLUMNS];
-    double average[6], highest = 0, compare = 0;
-    int year = 0;
+    double average[ROWS], highest = 0, compare = 0;
+    int year = 0, swimTemp[5], swimTempAvg = 0;
     in = fopen("CTextFiles\\ontario.csv", "r");
 
     char scanner[SIZE];
@@ -34,22 +34,35 @@ int main(void) {
             if (token != NULL) {
                 temperature[i][j] = atof(token); //converts the string to a float/double
 
-                switch(j) {
+                /*switch(j) {
                     case 25: average[0] += temperature[i][j]; break; //2020
                     case 26: average[1] += temperature[i][j]; break; //2021
                     case 27: average[2] += temperature[i][j]; break; //2022
                     case 28: average[3] += temperature[i][j]; break; //2023
                     case 29: average[4] += temperature[i][j]; break; //2024
                     case 30: average[5] += temperature[i][j]; break; //2025
-                }
+                }*/
 
-                if (i == 184) {
+                average[i] += temperature[i][j];
+
+                /*if (i == 184) {
                     compare = temperature[i][j]; //sets the next value to be compared
                     if (compare > highest) { //if the new comparable value is greater than the current highest, it becomes the new highest
                         highest = compare;
                         year = j; //the year is also stored.
                     }
+                }*/
+
+                if (temperature[i][j] > 20) {
+                    switch(j) {
+                        case 26: swimTemp[0]++; break;
+                        case 27: swimTemp[1]++; break;
+                        case 28: swimTemp[2]++; break;
+                        case 29: swimTemp[3]++; break;
+                        case 30: swimTemp[4]++; break;
+                    }
                 }
+
             } else {
                 temperature[i][j] = 0; // if the token is empty, it fills the cell with 0
             }
@@ -59,13 +72,18 @@ int main(void) {
     }
 
     fclose(in);
+
+    swimTempAvg = swimTemp[0] + swimTemp[1] + swimTemp[2] + swimTemp[3] + swimTemp[4];
     
-    for (int i = 0; i < 6; i++) { 
+    printf("\nYear           Average Water Temperature (Degrees Celsius)");
+
+    for (int i = 0; i < COLUMNS; i++) { 
         average[i] /= ROWS; //determines the average by dividing by the number of days (i.e. ROWS)
-        printf("The average of the temperatures for the year %d is %.2lf.\n", i + 2020, average[i]); //printing each average
+        printf("\n%d: %.2lf.", i + 1995, average[i]); //printing each average
     }
 
-    printf("The year with the highest temperature on day 185 was %d, with a recorded temperature of %.2lf.", year + 1995, highest); 
+    printf("\nThe year with the highest temperature on day 185 was %d, with a recorded temperature of %.2lf."
+    "\nNumber of days where swimming is possible:\n2021: %d\n2022: %d\n2023: %d\n2024: %d\n2025: %d", year + 1995, highest, swimTemp[0], swimTemp[1], swimTemp[2], swimTemp[3], swimTemp[4]); 
 
     return 0;
 }
